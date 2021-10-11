2d21756366803173f7d048eea378ad00f4504bfe
AdarshGrewal <adarshgrewal@gmail.com>
2021-10-11 10:10:49 +0530

    [DNM]thyme: allow building with PREBUILT_ELF_FILES

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -11,6 +11,7 @@ DEVICE_PATH := device/blackshark/penrose
 PENROSE_PREBUILT := device/blackshark/penrose-prebuilt
 
 BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
 # A/B
 AB_OTA_UPDATER := true
