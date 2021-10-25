c01f65f62b8b62cef4aca289171a2b22b71eee71
baolong24 <1667375308@qq.com>
2021-10-25 13:49:42 +0000

    penrose: Drop WFD

--- device.mk
+++ device.mk
@@ -168,16 +168,3 @@ PRODUCT_COPY_FILES += \
 # Vendor boot modules
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/prebuilt/modules/,$(TARGET_COPY_OUT_VENDOR_RAMDISK)/lib/modules)
-
-# WiFi Display
-PRODUCT_PACKAGES += \
-    libavservices_minijail \
-    libdisplayconfig.qti \
-    libminijail \
-    libnl \
-    libqdMetaData \
-    libwfdaac_vendor \
-    vendor.display.config@2.0
-
-PRODUCT_BOOT_JARS += \
-    WfdCommon
