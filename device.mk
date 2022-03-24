04c332408995f6b75b141f85426fcaa5d1cd5aa4
ganeshi4u <ganeshvarma@arrowos.net>
2022-03-24 11:21:05 +0800

    penrose: overlays: Give RRO's its own folder

--- device.mk
+++ device.mk
@@ -79,6 +79,10 @@ PRODUCT_PACKAGES += \
     com.android.nfc_extras
 
 # Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+
+# RRO Overlays
 PRODUCT_PACKAGES += \
     FrameworkResOverlayPenrose \
     WifiResOverlayPenrose \
